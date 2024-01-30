134f712e97e80e9150649b42d280cf9051fc3b14
aarn-hs <aarn.hs@gmail.com>
2024-01-29 22:31:32 -0600

    feat: add form

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Tripleten web_project_around
+# Proyecto 5
+
+## Tripleten web_project_around
+
+Este es el proyecto 5 del curso de desarrollador web de TripleTen. Mayormente esta hecho para practicar habilidades de javascript básico pero también se utiliza posicionamiento con flexbox y grid, al igual que diseño web adaptativo mediante el uso de media queries.
+
+Enlace: https://aarn-hs.github.io/web_project_around/
--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,33 @@
 const overlay = document.querySelector(".overlay");
 
 const popup = document.querySelector(".popup");
+const popupForm = popup.querySelector(".popup__container");
 const closeButton = popup.querySelector(".popup__closeIcon");
+const popupName = popup.querySelector(".popup__name");
+const popupAboutMe = popup.querySelector(".popup__aboutMe");
 
 const profileContainer = document.querySelector(".profile__container");
 const editButton = profileContainer.querySelector(".profile__editButton");
+const profileName = profileContainer.querySelector(".profile__name");
+const profileOccupation = profileContainer.querySelector(
+  ".profile__occupation"
+);
 
 function handlePopup() {
   popup.classList.toggle("popup_opened");
   overlay.classList.toggle("overlay_deployed");
+  popupName.value = profileName.textContent;
+  popupAboutMe.value = profileOccupation.textContent;
+}
+
+function handlePopupFormSubmit(evt) {
+  evt.preventDefault();
+  let popupName = popup.querySelector(".popup__name");
+  let popupAboutMe = popup.querySelector(".popup__aboutMe");
+  profileName.value = popupName.textContent;
+  profileOccupation.value = popupAboutMe.textContent;
 }
 
 editButton.addEventListener("click", handlePopup);
 closeButton.addEventListener("click", handlePopup);
+popupForm.addEventListener("submit", handlePopupFormSubmit);
