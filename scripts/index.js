fa4f343ff17d272ef7a509d0d34acd9a02a4fb00
aarn-hs <aarn.hs@gmail.com>
2024-02-29 23:22:48 -0600

    feat: add trashbutton functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,13 @@
 const overlay = document.querySelector(".overlay");
 
+//Popup consts
 const popup = document.querySelector(".popup");
 const popupForm = popup.querySelector(".popup__container");
 const closeButton = popup.querySelector(".popup__closeIcon");
 const popupName = popup.querySelector(".popup__name");
 const popupAboutMe = popup.querySelector(".popup__aboutMe");
 
+//Profile consts
 const profileContainer = document.querySelector(".profile__container");
 const editButton = profileContainer.querySelector(".profile__editButton");
 const profileName = profileContainer.querySelector(".profile__name");
@@ -13,9 +15,12 @@ const profileOccupation = profileContainer.querySelector(
   ".profile__occupation"
 );
 
+//Card conts
 const card = document.querySelector(".places__item");
 const likeButton = card.querySelector(".places__likeButton");
+const trashButton = card.querySelector(".places__trash");
 
+//Popup functions
 function handlePopup() {
   popup.classList.toggle("popup_opened");
   overlay.classList.toggle("overlay_deployed");
@@ -31,10 +36,17 @@ function handlePopupFormSubmit(evt) {
   overlay.classList.toggle("overlay_deployed");
 }
 
+//Popup listeners
 editButton.addEventListener("click", handlePopup);
 closeButton.addEventListener("click", handlePopup);
 popupForm.addEventListener("submit", handlePopupFormSubmit);
+
+//Card listeners
 likeButton.addEventListener("click", function (evt) {
   const eventTarget = evt.target;
   eventTarget.classList.toggle("places__likeButton_active");
+});
+trashButton.addEventListener("click", function () {
+  const gonerCard = trashButton.closest(".places__item");
+  gonerCard.remove();
 });
