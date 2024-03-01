c48647d648dcdbe3e7ce114200291adc920948f7
aarn-hs <aarn.hs@gmail.com>
2024-03-01 00:08:07 -0600

    feat: add newplace form functionality

--- scripts/index.js
+++ scripts/index.js
@@ -14,6 +14,17 @@ const profileName = profileContainer.querySelector(".profile__name");
 const profileOccupation = profileContainer.querySelector(
   ".profile__occupation"
 );
+const addButton = document.querySelector(".profile__addButton");
+const addButtonLong = document.querySelector(".profile__addButtonLong");
+
+//AddPlace Consts
+const addPlacePopup = document.querySelector(".placePopup");
+const addPlaceForm = addPlacePopup.querySelector(".placePopup__container");
+const addPlaceCloseButton = addPlacePopup.querySelector(
+  ".placePopup__closeIcon"
+);
+const addPlaceTitle = addPlacePopup.querySelector(".placePopup__name");
+const addPlacePic = addPlacePopup.querySelector(".placePopup__link");
 
 //Image popup consts
 const imagePopup = document.querySelector(".imagePopup");
@@ -37,14 +48,32 @@ function handlePopupFormSubmit(evt) {
   evt.preventDefault();
   profileName.textContent = popupName.value;
   profileOccupation.textContent = popupAboutMe.value;
-  popup.classList.toggle("popup_opened");
+  popup.classList.remove("popup_opened");
+  overlay.classList.remove("overlay_deployed");
+}
+
+//AddPlace functions
+function handleAddPlacePopup() {
+  addPlacePopup.classList.toggle("placePopup_opened");
   overlay.classList.toggle("overlay_deployed");
+}
+
+function handleAddPlacePopupFormSubmit() {
+  evt.preventDefault();
+  addPlacePopup.classList.remove("placePopup_opened");
+  overlay.classList.remove("overlay_deployed");
 }
 
 //Popup listeners
 editButton.addEventListener("click", handlePopup);
 closeButton.addEventListener("click", handlePopup);
 popupForm.addEventListener("submit", handlePopupFormSubmit);
+
+//AddPlacePopup listeners
+addButton.addEventListener("click", handleAddPlacePopup);
+addButtonLong.addEventListener("click", handleAddPlacePopup);
+addPlaceCloseButton.addEventListener("click", handleAddPlacePopup);
+addPlaceForm.addEventListener("submit", handleAddPlacePopupFormSubmit);
 
 //Card listeners
 likeButton.addEventListener("click", function (evt) {
